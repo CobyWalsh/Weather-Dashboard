c56e11ebba169e9e10a5f7a0d5662de1ec6479ee
coby walsh <cobywalsh7@yahoo.com>
2023-05-18 18:04:03 -0600

    added bootstrap via css and javascript to html file

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
 var apiKey = "93d36d5ed8f49e95ac5c409eb3d39964";
 var city;
 var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
+
+fetch(queryUrl);
