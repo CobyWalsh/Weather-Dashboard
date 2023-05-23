18d8a28cb2220b82d658962ed683d0ec7326a17c
coby walsh <cobywalsh7@yahoo.com>
2023-05-23 07:36:41 -0600

    added local storage to search weather data

--- Assets/script.js
+++ Assets/script.js
@@ -1,55 +1,47 @@
-var apiKey = "93d36d5ed8f49e95ac5c409eb3d39964";
-var city;
-var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={93d36d5ed8f49e95ac5c409eb3d39964}'
-var searchWeather = document.getElementById("searchweather")
+const savedWeather = handleLocalStorage("weather");
+const apiKey = "93d36d5ed8f49e95ac5c409eb3d39964"
+const queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={93d36d5ed8f49e95ac5c409eb3d39964}'
+
 
 
 function handleSearch(event) {
+    event.preventDefault()
+    const searchWeather = document.getElementById("searchweather").value
     handleLocalStorage("searchweather", "set")
     // call function on form submit
-    document.getElementById("searchweather").addEventListener("submit", myFunction);
-    fetch(queryUrl)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchWeather}&appid=${apiKey}`)
         // return response .json
-        .then(function () {
+        .then(function (response) {
             return response.json()
 
         })
         .then(function (data) {
+            console.log(data);
+            localStorage.setItem("searchWeather", data);
             // dynamically create elements with data and display on page 
-            var response = document.createDocumentFragment();
-            var link = document.createElement("a");
-            link.setAttribute("id", "id1");
-            link.setAttribute("href", "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={93d36d5ed8f49e95ac5c409eb3d39964}")
-
+            // set into local storage
+            // display data on webpage
         })
-    link.appendChild(document.createTextNode("linkText"));
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "id2");
-    div.appendChild(document.createTextNode("divText"));
-
-    response.appendChild(link);
-    response.appendChild(div);
-    document.getElementById("main").appendChild(response);
 }
-try {
-        .catch(function()) {
 
-            function handleLocalStorage(searchKey, type) {
-            if (type === "get") {
-                // need to get from local storage and display on page (buttons) addeventlistener
-                localStorage.setItem("get", "set");
-
-            } else if (type === "set") {
-                // check array if search key exists if not add to local storage
-            }
-
-
-        }
+function handleLocalStorage(searchKey, type) {
+    var searches = []
+    
+    if (type === "get") {
+        
+        searches = localStorage.getItem("searches");
+      //  write code to add searches to the resent search section
+    } else if (type === "set") { 
+        searches.push(searchKey)
+        
+        localStorage.setItem("searches", searches);
+        handleLocalStorage("", "get")
     }
-    // getting saved inputs on load
-    handleLocalStorage("", "get")
 
-     //addadvent listener on form submit call handle search
-     addEventListener(submit, handleSearch);
+
 }
+// getting saved inputs on load
+handleLocalStorage("", "get")
+
+//addadvent listener on form submit call handle search
+document.getElementById("search").addEventListener("click", handleSearch);
